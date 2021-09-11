a37b58f87ca002991f449e09359ea88c4a97675d
Isadora Soares <isadoragalvaoss@gmail.com>
2021-09-11 15:04:39 -0300

    feat: create map and get user location
    Co-authored-by: gabrieldavi <gdavi.gd@gmail.com>
    Co-authored-by: ingridSCarvalho <ingridSCarvalho@users.noreply.github.com>

--- src/screens/MapScreen/index.jsx
+++ src/screens/MapScreen/index.jsx
@@ -1,6 +1,52 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
+import styles from './styles';
+import markerImage from '../../assets/images/marker.png';
 
 export function MapScreen() {
-  return <Text>MapScreen</Text>;
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    async function getPosition() {
+      const { status } = await requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+
+      const currentLocation = await getCurrentPositionAsync({});
+
+      setLocation(currentLocation.coords);
+    }
+    getPosition();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+    
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: -27.5965432,
+              longitude: -48.5865547,
+            }}
+            image={markerImage}
+          />
+        </MapView>
+      )}
+    </View>
+  );
 }
---
+++ src/screens/MapScreen/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default styles;
