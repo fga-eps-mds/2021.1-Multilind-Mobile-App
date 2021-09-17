1ab34d8fa9a701167e175ee8a21800153739fcff
isadoragalvaoss <isadoragalvaoss@gmail.com>
2021-09-17 20:54:59 -0300

    feat: add language screen
    Co-authored-by: Helder Lourenço de Abreu Marques <F1reFinger@users.noreply.github.com>

--- src/screens/LanguageScreen/index.jsx
+++ src/screens/LanguageScreen/index.jsx
@@ -1,22 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Text, View, ScrollView } from 'react-native';
-import { Button, Input } from '../../components';
+import { Input } from '../../components';
 import Languages from '../../languages.json';
 import { Entypo } from '@expo/vector-icons';
-
+import { AntDesign } from '@expo/vector-icons';
 import styles from './styles';
-import { DARK_GRAY, MONTSERRAT_BOLD } from '../../constants';
+import {
+  DARK_GRAY,
+  MONTSERRAT_BOLD,
+  MONTSERRAT_SEMIBOLD,
+} from '../../constants';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export function LanguageScreen() {
-  const linguas = Languages.languages;
+  const languages = Languages.languages;
+  const navigation = useNavigation();
 
-  console.log(linguas);
   const list = () => {
-    return linguas.map((users) => {
+    return languages.map((language) => {
       return (
-        <View key={users.id} style={{ margin: 10 }}>
-          <Text>{users.name}</Text>
+        <View
+          key={language.id}
+          style={{
+            margin: 1,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('LanguageInitial', { language });
+            }}
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 0,
+              paddingVertical: 20,
+              paddingHorizontal: 25,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text
+              style={{
+                color: 'black',
+                fontSize: 18,
+                fontFamily: MONTSERRAT_SEMIBOLD,
+              }}
+            >
+              {language.name}
+            </Text>
+            <AntDesign
+              name="right"
+              size={24}
+              color="#B1B1B1"
+              style={{ paddingRight: 14 }}
+            />
+          </TouchableOpacity>
         </View>
       );
     });
@@ -44,7 +84,14 @@ export function LanguageScreen() {
         </Text>
       </View>
       <Input
-        icon={<Entypo name="magnifying-glass" size={30} color={DARK_GRAY} />}
+        icon={
+          <Entypo
+            name="magnifying-glass"
+            size={30}
+            color={DARK_GRAY}
+            style={{ left: 10 }}
+          />
+        }
         inputContainerStyle={[
           styles.searchBar,
           {
@@ -53,9 +100,7 @@ export function LanguageScreen() {
         ]}
         placeholder="Pesquisar língua"
       />
-      <ScrollView style={{ top: 70 }}>
-        <Button>{list()}</Button>
-      </ScrollView>
+      <ScrollView style={{ top: 70 }}>{list()}</ScrollView>
     </View>
   );
 }
