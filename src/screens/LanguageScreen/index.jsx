bc37df042c60f03805792d500b2340e5283852fa
isadoragalvaoss <isadoragalvaoss@gmail.com>
2021-09-17 22:05:33 -0300

    feat: add sort language
    Co-authored-by: Helder Loureço <180121847@aluno.unb.br>

--- src/screens/LanguageScreen/index.jsx
+++ src/screens/LanguageScreen/index.jsx
@@ -13,53 +13,56 @@ import {
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { FontAwesome } from '@expo/vector-icons';
 
 export function LanguageScreen() {
   const languages = Languages.languages;
   const navigation = useNavigation();
 
   const list = () => {
-    return languages.map((language) => {
-      return (
-        <View
-          key={language.id}
-          style={{
-            margin: 1,
-          }}
-        >
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('LanguageInitial', { language });
-            }}
+    return languages
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((language) => {
+        return (
+          <View
+            key={language.id}
             style={{
-              backgroundColor: 'white',
-              borderRadius: 0,
-              paddingVertical: 20,
-              paddingHorizontal: 25,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
+              margin: 1,
             }}
           >
-            <Text
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('LanguageInitial', { language });
+              }}
               style={{
-                color: 'black',
-                fontSize: 18,
-                fontFamily: MONTSERRAT_SEMIBOLD,
+                backgroundColor: 'white',
+                borderRadius: 0,
+                paddingVertical: 20,
+                paddingHorizontal: 25,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
               }}
             >
-              {language.name}
-            </Text>
-            <AntDesign
-              name="right"
-              size={24}
-              color="#B1B1B1"
-              style={{ paddingRight: 14 }}
-            />
-          </TouchableOpacity>
-        </View>
-      );
-    });
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 18,
+                  fontFamily: MONTSERRAT_SEMIBOLD,
+                }}
+              >
+                {language.name}
+              </Text>
+              <AntDesign
+                name="right"
+                size={24}
+                color="#B1B1B1"
+                style={{ paddingRight: 14 }}
+              />
+            </TouchableOpacity>
+          </View>
+        );
+      });
   };
 
   const insets = useSafeAreaInsets();
@@ -83,24 +86,35 @@ export function LanguageScreen() {
           Línguas Indígenas
         </Text>
       </View>
-      <Input
-        icon={
-          <Entypo
-            name="magnifying-glass"
-            size={30}
+      <View>
+        <Input
+          icon={
+            <Entypo
+              name="magnifying-glass"
+              size={30}
+              color={DARK_GRAY}
+              style={{ left: 10 }}
+            />
+          }
+          inputContainerStyle={[
+            styles.searchBar,
+            {
+              top: insets.top + 20,
+            },
+          ]}
+          placeholder="Pesquisar língua"
+        />
+        <TouchableOpacity style={{ top: 15 }}>
+          <FontAwesome
+            name="filter"
+            size={24}
             color={DARK_GRAY}
-            style={{ left: 10 }}
+            style={{ left: 350 }}
           />
-        }
-        inputContainerStyle={[
-          styles.searchBar,
-          {
-            top: insets.top + 20,
-          },
-        ]}
-        placeholder="Pesquisar língua"
-      />
-      <ScrollView style={{ top: 70 }}>{list()}</ScrollView>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView style={{ top: 50 }}>{list()}</ScrollView>
     </View>
   );
 }
