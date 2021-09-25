54337e6231b70fa50cb19d87ee861107ca6384a8
CarlosZoft <carlosoftwar@gmail.com>
2021-09-25 18:21:33 -0300

    feat : add list language component
    Co-authored-by: Isadora <isadoragalvaoss@gmail.com>

---
+++ src/components/ListLanguages/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+export function ListLanguages({ listLanguage }) {
+  const navigation = useNavigation();
+
+  return listLanguage.map((language, index) => (
+    <View key={index} style={styles.listcontainer}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('LanguageInitial', {
+            language: {
+              id: language.id_lingua,
+              name: language.nome,
+              troncolinguistico: language.id_tronco,
+              regioesfaladas: language.regioesfaladas
+                ? language.regioesfaladas
+                : 'algo',
+            },
+          });
+        }}
+        style={styles.list}
+      >
+        <Text style={styles.textlist}>{language.nome}</Text>
+        <AntDesign
+          name="right"
+          size={24}
+          color="#B1B1B1"
+          style={styles.arrow}
+        />
+      </TouchableOpacity>
+    </View>
+  ));
+}
---
+++ src/components/ListLanguages/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+import { MONTSERRAT_SEMIBOLD } from '../../constants';
+
+const styles = StyleSheet.create({
+  listcontainer: {
+    margin: 1,
+  },
+  list: {
+    backgroundColor: 'white',
+    borderRadius: 0,
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textlist: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: MONTSERRAT_SEMIBOLD,
+  },
+  arrow: {
+    paddingRight: 14,
+  },
+});
+
+export default styles;
