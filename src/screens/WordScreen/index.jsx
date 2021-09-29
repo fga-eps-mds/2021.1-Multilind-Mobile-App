0ee589b2383c9985fc557dbcca5169564ccff2f5
hericklima22 <herick.lima77@gmail.com>
2021-09-28 23:55:22 -0300

    feat: words list sort by alphabet letters
    Co-authored-by: ingridSCarvalho <ingridSCarvalho@users.noreply.github.com>

--- src/screens/WordScreen/index.jsx
+++ src/screens/WordScreen/index.jsx
@@ -1,147 +1,41 @@
 import React, { useState } from 'react';
-import { Text, View, ScrollView, Modal, TouchableOpacity } from 'react-native';
-import {
-  Entypo,
-  FontAwesome5,
-  AntDesign,
-  FontAwesome,
-} from '@expo/vector-icons';
-// import { useRoute, useNavigation } from '@react-navigation/native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { Entypo, AntDesign, FontAwesome } from '@expo/vector-icons';
+import { useRoute, useNavigation } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
-import {
-  DARK_GRAY,
-  MONTSERRAT_BOLD,
-  SCREEN_HEIGHT,
-  SCREEN_WIDTH,
-} from '../../constants';
+import { DARK_GRAY, MONTSERRAT_BOLD } from '../../constants';
 import styles from './styles';
 import { useWord } from '../../contexts';
 import { Input } from '../../components';
 
 export function WordsScreen() {
-  // const route = useRoute();
-  // const wordID = route.params?.alphabetParams;
+  const route = useRoute();
+  const letter = route.params?.alphabetParam.name;
+  const language = route.params?.language;
   const { words } = useWord();
-  const [listWord, setListWord] = useState(words);
-
+  console.log('language', language);
   const list = () =>
-    listWord.map((word) => (
-      <View key={word.id_palavra} styçle={styles.listcontainer}>
-                
-        <TouchableOpacity style={styles.list}>
-          <Text style={styles.textlist}>{word.nome}</Text>
-          <AntDesign
-            name="right"
-            size={24}
-            color="#B1B1B1"
-            style={styles.arrow}
-          />
-                  
-        </TouchableOpacity>
-              
-      </View>
-    ));
-
+    words
+      .filter((word) => word.nome.substr(0, 1) === letter)
+      .map((word) => (
+        <View key={word.id_palavra} style={styles.listcontainer}>
+          <TouchableOpacity style={styles.list}>
+            <Text style={styles.textlist}>{word.nome}</Text>
+            <AntDesign
+              name="right"
+              size={24}
+              color="#B1B1B1"
+              style={styles.arrow}
+            />
+          </TouchableOpacity>
+        </View>
+      ));
   const insets = useSafeAreaInsets();
-  const [visib, setVisib] = useState(false);
-  const sortList = () => {
-    const newList = [...words];
-
-    newList.sort((a, b) => a.nome.localeCompare(b.nome));
-    setListWord(newList);
-  };
-  const notsortList = () => {
-    const newList = [...words];
-
-    newList.sort((a, b) => b.nome.localeCompare(a.nome));
-    setListWord(newList);
-  };
+  const [, setVisib] = useState(false);
   return (
     <>
-            
-      <Modal visible={visib} transparent animationType="fade">
-                
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => {
-            setVisib(false);
-          }}
-          style={{
-            width: SCREEN_WIDTH,
-            height: SCREEN_HEIGHT,
-            backgroundColor: 'rgba(0, 0, 0, 0, 3)',
-          }}
-        >
-                    
-          <TouchableWithoutFeedback>
-                        
-            <View
-              style={{
-                marginTop: 140,
-                marginLeft: SCREEN_WIDTH - 233,
-                backgroundColor: 'white',
-                width: 233,
-                padding: 10,
-              }}
-            >
-                            
-              <Pressable
-                style={styles.flex}
-                onPress={() => {
-                  sortList();
-                  setVisib(false);
-                }}
-              >
-                                
-                <FontAwesome5
-                  name="sort-alpha-down"
-                  size={30}
-                  color="black"
-                  style={{ left: 5 }}
-                />
-                                
-                <Text style={styles.textmodal}>
-                                    Listar por nome (crescente)                 
-                </Text>
-                              
-              </Pressable>
-                            
-              <Pressable
-                style={styles.flex}
-                onPress={() => {
-                  notsortList();
-                  setVisib(false);
-                }}
-              >
-                                
-                <FontAwesome5
-                  name="sort-alpha-down-alt"
-                  size={30}
-                  color="black"
-                  style={styles.iconmodal}
-                />
-                                
-                <Text style={styles.textmodal}>
-                                    Listar por nome (decresente)
-                                  
-                </Text>
-                              
-              </Pressable>
-                          
-            </View>
-                      
-          </TouchableWithoutFeedback>
-                  
-        </TouchableOpacity>
-              
-      </Modal>
-            
       <View>
-                
         <View style={styles.container}>
-                    
           <Text
             style={{
               marginTop: insets.top + 10,
@@ -149,15 +43,11 @@ export function WordsScreen() {
               fontSize: 30,
             }}
           >
-                        Palavras Indígenas           
+            Palavras Indígenas
           </Text>
-                  
         </View>
-              
       </View>
-            
       <View>
-                
         <Input
           icon={
             <Entypo
@@ -175,26 +65,22 @@ export function WordsScreen() {
           ]}
           placeholder="Pesquisar palavra"
         />
-                
+
         <TouchableOpacity
           style={{ top: 15 }}
           onPress={() => {
             setVisib(true);
           }}
         >
-                    
           <FontAwesome
             name="filter"
             size={24}
             color={DARK_GRAY}
             style={{ left: '80%' }}
           />
-                  
         </TouchableOpacity>
-              
       </View>
             <ScrollView style={{ top: '4%' }}>{list()}</ScrollView>
-          
     </>
   );
 }
