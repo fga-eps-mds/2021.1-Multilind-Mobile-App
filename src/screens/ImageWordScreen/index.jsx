e502e692c84e07140693d26d6a8aa68f44de6831
CarlosZoft <carlosoftwar@gmail.com>
2021-09-29 22:53:37 -0300

    feat: add images of words with scroll view
    Co-authored-by: isadoragalvaoss <isadoragalvaoss@users.noreply.github.com>

--- src/screens/ImageWordScreen/index.jsx
+++ src/screens/ImageWordScreen/index.jsx
@@ -1,27 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
-import { View, Image } from 'react-native';
+import { View, Image, Text, ScrollView } from 'react-native';
+import { WordService } from '../../services';
 import { TopBar, GoBack, ImageContainer } from '../../components';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles';
+import abacateIcon from '../../abacate-600x600.jpg';
 
 export function ImageWordScreen() {
   const route = useRoute();
   const language = route.params?.language;
-  const nomes = ['isa', 'carlos', 'qlqcoisa', 'piseiro'];
+  const [word, setWord] = useState([]);
+
+  useEffect(() => {
+    async function getWords() {
+      const response = await WordService.getAllWords(language.id_lingua);
+      setWord(response[0].palavras);
+    }
+    getWords();
+  }, []);
+
+  const list = () =>
+    word.length
+      ? word.map((currentWord, index) => (
+          <View key={index} style={styles.containerImage}>
+            <ImageContainer palavra={currentWord.nome} image={abacateIcon} />
+          </View>
+        ))
+      : null;
+
   return (
     <SafeAreaView>
       <GoBack />
-      <TopBar>{language.name}</TopBar>
-      <View style={styles.content}>
-        <SafeAreaView style={styles.container}>
-          {nomes.map((el) => (
-            <View style={styles.containerImage}>
-              <ImageContainer palavra={el} />
-            </View>
-          ))}
-        </SafeAreaView>
-      </View>
+      <TopBar>{language.nome}</TopBar>
+
+      <ScrollView>
+        <View style={styles.content}>
+          <SafeAreaView style={styles.container}>{list()}</SafeAreaView>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
