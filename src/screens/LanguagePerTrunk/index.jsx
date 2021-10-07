2c60d0b95d7fe3e3cbe6e92130a3e3a1e7f8bbe6
CarlosZoft <carlosoftwar@gmail.com>
2021-10-07 18:49:14 -0300

    feat: created filter and clear field of text input
    Co-authored-by: isadoragalvaoss <isadoragalvaoss@users.noreply.github.com>

--- src/screens/LanguagePerTrunk/index.jsx
+++ src/screens/LanguagePerTrunk/index.jsx
@@ -14,47 +14,59 @@ import styles from './styles';
 
 export function LanguagePerTrunk() {
   const { trunks } = useTrunk();
-  const [listLanguage, setListLanguage] = useState(trunks);
+  const [listTrunk, setListLanguage] = useState(trunks);
+  const [trunkSearch, setTrunkSearch] = useState('');
   const [expanded, setExpanded] = useState(false);
   const [visib, setVisib] = useState(false);
 
+  const updateSearch = (event) => setTrunkSearch(event);
   const updateLanguage = (value) => setListLanguage(value);
   const updateVisib = (value) => setVisib(value);
   const toggleExpand = (index) =>
     setExpanded(expanded === index ? null : index);
 
   const list = () =>
-    listLanguage.map((tronco, index) => (
-      <View key={tronco.id_tronco} style={styles.listcontainer}>
-        <TouchableOpacity
-          style={styles.list}
-          onPress={() => toggleExpand(index)}
-        >
-          <View style={styles.troncoLinguas}>
-            <Text style={styles.textlist}>{tronco.nome}</Text>
+    listTrunk
+      .filter((tronco) =>
+        trunkSearch === '' ||
+        tronco.nome.toLowerCase().substring(0, trunkSearch.length) ===
+          trunkSearch.toLowerCase()
+          ? tronco
+          : false
+      )
+      .map((tronco, index) => (
+        <View key={tronco.id_tronco} style={styles.listcontainer}>
+          <TouchableOpacity
+            style={styles.list}
+            onPress={() => toggleExpand(index)}
+          >
+            <View style={styles.troncoLinguas}>
+              <Text style={styles.textlist}>{tronco.nome}</Text>
+              {tronco.linguas.length > 0 && (
+                <Text style={styles.qtdLinguas}>
+                  {`${tronco.linguas.length} línguas`}
+                </Text>
+              )}
+            </View>
             {tronco.linguas.length > 0 && (
-              <Text style={styles.qtdLinguas}>
-                {`${tronco.linguas.length} línguas`}
-              </Text>
+              <AntDesign
+                name={expanded === index ? 'up' : 'down'}
+                size={24}
+                color="#B1B1B1"
+                style={styles.arrow}
+              />
             )}
-          </View>
-          {tronco.linguas.length > 0 && (
-            <AntDesign
-              name={expanded === index ? 'up' : 'down'}
-              size={24}
-              color="#B1B1B1"
-              style={styles.arrow}
-            />
+          </TouchableOpacity>
+          {expanded === index && (
+            <ListLanguages listLanguage={tronco.linguas} />
           )}
-        </TouchableOpacity>
-        {expanded === index && <ListLanguages listLanguage={tronco.linguas} />}
-      </View>
-    ));
+        </View>
+      ));
 
   return (
     <SafeAreaView>
       <ModalMod
-        list={listLanguage}
+        list={listTrunk}
         onChange={updateLanguage}
         onChangeV={updateVisib}
         visual={visib}
@@ -62,7 +74,10 @@ export function LanguagePerTrunk() {
       <View>
         <TopBar>Línguas Indígenas</TopBar>
         <View>
-          <SearchBar placeholder="Pesquisar por um Tronco" />
+          <SearchBar
+            placeholder="Pesquisar por um Tronco"
+            onChange={updateSearch}
+          />
           <Filter onChangeV={updateVisib} />
         </View>
         <ScrollView style={styles.scrollView}>{list()}</ScrollView>
