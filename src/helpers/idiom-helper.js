588abdb6afd50a1db2eaab164efdaec1f4cdb78a
gabrieldvpereira <gdavi.gd@gmail.com>
2021-10-11 17:31:18 -0300

    feat:  change default name for trunk

--- src/helpers/idiom-helper.js
+++ src/helpers/idiom-helper.js
@@ -2,7 +2,7 @@ export const normalizeSpokeResponse = (idiom) =>
   idiom.map((data) => ({
     id: data.id_conteudo,
     title: data.lingua.nome,
-    origin: data.lingua.tronco?.nome || 'Não indentificado',
+    origin: data.lingua.tronco?.nome || 'Isolada',
     ethnicities: data.lingua?.etnia?.map((e) => e.nome),
     latitude: data.localidade.latitude,
     longitude: data.localidade.longitude,
