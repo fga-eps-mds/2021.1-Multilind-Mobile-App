3836e3aaaee88a21ec08f8817389740e906e2a3a
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2021-11-10 00:19:28 -0300

    fix: fix search at word and language screen

--- src/utils/FilterListSearch.js
+++ src/utils/FilterListSearch.js
@@ -1,20 +1,7 @@
-export const FilterListSearch = (list, search) => {
+export const FilterListSearch = (list, search) =>
   list.filter((item) =>
     search === '' ||
     item.nome.toLowerCase().substring(0, search.length) === search.toLowerCase()
       ? item
       : false
   );
-};
-
-export const FilterListSearchWord = (list, search) => {
-  list.filter((item) =>
-    search === '' ||
-    item.nome.toLowerCase().substring(0, search.length) ===
-      search.toLowerCase() ||
-    item.significado.toLowerCase().substring(0, search.length) ===
-      search.toLowerCase()
-      ? item
-      : false
-  );
-};
